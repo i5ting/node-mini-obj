a8c2a8a2412d1315b5299a65a7f5796b8b1e4fbd
alfred sang <i5ting@126.com>
2014-12-08 17:00:48 +0800

    for use call inherit

---
+++ testinherit4.js
@@ -0,0 +1,29 @@
+var $ = require('./index')
+
+$('PersonBase',{
+	constructor: function(name) { 
+		console.log('---1----');
+		this.name = name;
+	}, 
+	bbb:'bbb',
+	hello:function(){
+		console.log('hello');
+	}
+});
+
+$('Person',{
+	constructor: function(name) { 
+		PersonBase.call(this);
+		this.name = name;
+	}, 
+	say: function(){
+		console.log('hello ' + this.name);
+		console.log(this.bbb);
+	}
+})
+
+var p = new Person('alfred sang');
+console.log(p.name);
+
+p.hello()
+p.say();
